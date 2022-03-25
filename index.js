19bc1bf574c0119dd69dcc18df5fb332f979aa60
Rutu Thakkar <rutu.thakkar@iviewlabs.net>
2022-03-25 17:39:45 +0530

    port change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const db = require("./models");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
